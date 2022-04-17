baecdea6ce7e1ef19978a5c4c950b3aab1f5d6cd
rosaainz <al2173040136@azc.uam.mx>
2022-04-17 16:59:39 -0500

    first object and interpolation

---
+++ weekly_mission_2/example1/example1_1.js
@@ -0,0 +1,4 @@
+console.log("Creación de un objeto vacío")
+const myFirstObject = "Heeey" //empty object
+console.log('1. Objeto vacío: ${myFirstObject}') //interpolacion de variables
+//console.log('1. Objeto vacío:'+myFirstObject)  //emprime lo mismo que la linea de arriba

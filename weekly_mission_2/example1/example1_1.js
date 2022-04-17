18c7aebe42486f1a7c7f097bd22ac9ce99504148
rosaainz <al2173040136@azc.uam.mx>
2022-04-17 17:03:03 -0500

    empty object

--- weekly_mission_2/example1/example1_1.js
+++ weekly_mission_2/example1/example1_1.js
@@ -1,4 +1,5 @@
 console.log("Creación de un objeto vacío")
-const myFirstObject = "Heeey" //empty object
-console.log('1. Objeto vacío: ${myFirstObject}') //interpolacion de variables
+const myFirstObject = {} //empty object
+console.log('1. Objeto vacío: ') //interpolacion de variables
 //console.log('1. Objeto vacío:'+myFirstObject)  //emprime lo mismo que la linea de arriba
+console.log(myFirstObject)

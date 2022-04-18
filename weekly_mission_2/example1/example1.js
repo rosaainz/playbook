03a107754f8f374f9bf6a6ed9a1f1fbf0f9374c0
rosaainz <al2173040136@azc.uam.mx>
2022-04-18 18:16:27 -0500

    ejercicio 2.4 live

--- weekly_mission_2/example1/example1.js
+++ weekly_mission_2/example1/example1.js
@@ -64,3 +64,29 @@ const explorers = [
 
 console.log(explorers)
 
+//Ejercicio 2 live
+console.log("Ejercicio 2.1")
+
+//usamos el metodo forEach, que nos permite recorrer 
+explorers.forEach(explorer => console.log(explorer)) //imprime los explorers completos, ya con missions
+
+
+//para imprimir solo una carcateristica:
+explorers.forEach(explorer => console.log(explorer.name))
+//objeto.forEach(nombreYoleDe => console.log(nombreYoleDe.caracteristica)))
+
+
+console.log("Ejercicio 2.2")
+explorers.forEach(item => console.log(item.stack))
+
+//crea una lista de listas usando el metodo map
+//va a la lista de explorers, trae el stack y los pone en una nueva lista
+console.log("Ejercicio 2.3")
+const explorers_stacks = explorers.map(my_explorer_in_list => (my_explorer_in_list.stack)) //map ayuda a crear una nueva lista apartir de otra, para extraer info
+console.log(explorers_stacks) 
+
+
+//Obten la lista de los explorers que tengan en su lista "js" 
+console.log("Ejercicio 2.4")
+const explorers_with_js = explorers.filter(my_explorer_in_list => my_explorer_in_list.stack.includes("js")) //filter tambien trae info de otra lista y la guarda en una nueva E INCLUDES, hace que busque especificamente 
+console.log(explorers_with_js)

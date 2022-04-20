9f70d42670d28c194a82c411bd9c98c3dcc7c539
rosaainz <al2173040136@azc.uam.mx>
2022-04-19 21:47:17 -0500

    Plataformas: twitter, facebook, uber

--- weekly_mission_2/exercises/ejercicio_1/ejercicio1.js
+++ weekly_mission_2/exercises/ejercicio_1/ejercicio1.js
@@ -25,6 +25,10 @@ console.log(repo.getGeneralInfo())
 
 
 
+//////////////////////////
+
+
+
 //2. Issue y PullRequest
 const issue = {
     title: '05 Live 2 Semana 2 [Miercoles 13 de Abril]',
@@ -66,3 +70,48 @@ console.log('/// Ejercicio 1 /// PullRequest')
 console.log("Titulo del PR: " +pullRequest.title)
 console.log("Status del PR: " +pullRequest.getStatus())
 console.log(pullRequest.getTitleAndAutor())
+
+
+/////////////
+
+//Twitter:
+
+const twitter = {
+    user: {
+        user: 'rosa',
+        username: 'rosaainz',
+        followers: 303,
+        dateCreated: 'julio 2011'
+    },
+    trending_topic: 'Web Development',
+    hashtag: '#AlertaAmber'
+}
+
+//facebook:
+const facebook = {
+    user: {
+        user: 'rosa sainz',
+        post: '1200',
+        biography:'Bailo y canto frente al espejo'
+    }
+
+}
+
+//uber:
+const uber = {
+    profile: {
+        user: 'rosa sainz',
+        activity: 'user',
+        calification: '5 estrellas'
+    },
+    travel: '30 viajes'
+
+}
+console.log('/// Ejercicio 1 /// Plataformas')
+console.log("Twitter:")
+console.log(twitter)
+console.log("Facebook:")
+console.log(facebook)
+console.log("Uber:")
+console.log(uber)
+

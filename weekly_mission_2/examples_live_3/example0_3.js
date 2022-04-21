3431b3749edb5ca06e00d2a3acee5ad9dc9ad937
rosaainz <al2173040136@azc.uam.mx>
2022-04-21 12:21:54 -0500

    Ejemplo Modelado Live 3 sem 2

---
+++ weekly_mission_2/examples_live_3/example0_3.js
@@ -0,0 +1,57 @@
+// INSTAGRAM ¿que veo?
+
+/** Modelar la información que vemos:
+ * Perfil del usuario 
+ * - Nombre 
+ * - Usuario 
+ * - Seguidores
+ * - Seguidos
+ * - Numero de post
+ * - Bio  
+ */
+    const ig_user = {
+        name: "User Name",
+        username: "username",
+        followers: 100,
+        following: 20,
+        total_posts: 50
+    }
+/*
+ * Fotos del usuario
+ * - Imagen 
+ * - Likes
+ * - Fecha de creación 
+ * - Descripcion 
+ */
+    const ig_photo = {
+        photo: URL,
+        likes: acc,
+        dateCreation: "",
+        description: "",
+    }
+    
+/*
+* Mensajes
+ * - Usuario destinatario 
+ * - Lista de mensajes 
+ * Videos del usuario
+ * - Imagen 
+ * - Likes
+ * - Fecha de creación 
+ * - Descripcion 
+ * Reels del usuario
+ * - Imagen 
+ * - Likes
+ * - Fecha de creación 
+ * - Descripcion 
+ * Fotos etiquetadas 
+ * - Imagen 
+ * - Likes
+ * - Fecha de creación 
+ * - Descripcion 
+ * - Usuario que etiqueta 
+ *
+
+
+   
+

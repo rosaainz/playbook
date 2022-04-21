fc35c18ad041116e1fb6a98a605a5bddb35ce2a6
rosaainz <al2173040136@azc.uam.mx>
2022-04-21 13:36:23 -0500

    Clase vacia

---
+++ weekly_mission_2/examples_live_3/example2_1.js
@@ -0,0 +1,16 @@
+console.log("POO")
+
+// objeto de js   Abstraer informacion
+const woopa = {
+    name: "Woopa",
+    mission: "Node JS",
+    age: "12",
+    color: "pink"
+}
+
+// Crear una clase
+class Ajolonauta {  //la sintaxis va con inicial mayuscula 
+    //clase vacia 
+}
+
+console.log(Ajolonauta)

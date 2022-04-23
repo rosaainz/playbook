c0c6b8dbe7da12888313e68dc39d7b31c8c3d749
rosaainz <al2173040136@azc.uam.mx>
2022-04-23 11:10:45 -0500

    Intro TDD

--- weekly_mission_2/example5/pokemon.test.js
+++ weekly_mission_2/example5/pokemon.test.js
@@ -2,7 +2,13 @@ import Pokemon from './pokemon'   //importa una clase
 
 test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')   //instanciamos la clase (creamos un objeto)
-  expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+  expect(myPokemon.name).toBe('Pikachu'); // Prueba
 
   //expect(A).toBe(B);   compara A con B 
 });
+
+
+/// TDD test driven development 
+// 1. Escribir una prueba de lo que vamos a modelar
+// 2. Correr la prueba y verla fallar
+// 3. Escribir el código que haga que pase esa prueba 

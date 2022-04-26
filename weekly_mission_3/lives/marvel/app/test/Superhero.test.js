e970367653cfaa9395c8d0f3a48ee8f1d7321f50
rosaainz <al2173040136@azc.uam.mx>
2022-04-26 18:51:49 -0500

    4. Agregando prueba vacia

---
+++ weekly_mission_3/lives/marvel/app/test/Superhero.test.js
@@ -0,0 +1,6 @@
+describe("Test suite Dummy Description", () =>{
+    TextDecoderStream('Case 1 Dummy', () =>{
+        const resultOfSomething = 1 + 2
+        expect(resultOfSomething).toBe(18);
+    });
+})

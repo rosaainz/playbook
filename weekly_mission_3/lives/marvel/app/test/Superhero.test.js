d760214e402ca2142c5f4145f5eefa0a1de0420f
rosaainz <al2173040136@azc.uam.mx>
2022-04-26 19:25:18 -0500

    6. Creando prueba Superhero.test.js

--- weekly_mission_3/lives/marvel/app/test/Superhero.test.js
+++ weekly_mission_3/lives/marvel/app/test/Superhero.test.js
@@ -1,6 +1,14 @@
-describe("Test suite Dummy Description", () =>{
-    TextDecoderStream('Case 1 Dummy', () =>{
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
-    });
+const Superhero = require()
+
+describe("Unit Test for Superhero", () =>{
+	test('Case 1: Get a superhero', () => {
+		const ironman = new Superhero("Iron Man", "Tony Stark", "Robert Downey Jr.")
+		
+		expect(ironman.name).toBe("Iron Man")
+		expect(ironman.actor).toBe("Robert Downey Jr.")
+		expect(ironman.heroName).toBe("Tony Stark")
+ });
 })
+
+//Para realizar esta prueba necesitamos una clase llamada Superhero 
+// con las propiedades: name, actor y heroName
